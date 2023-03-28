d473b816e51917da993d32d886dbb36f388656e4
paarth-sharma <paarthsharma2003@gmail.com>
2023-03-28 20:49:36 +0530

    added assignment_1

--- assignment_1/ques_1.py
+++
@@ -1,9 +0,0 @@
-x = int(input("enter a integer:"))
-n = int(input("enter the no. of terms"))
-t = 1
-sum = 0
-for j in range (1,n+1):
-    t = (t*x)/j
-    sum = sum+t
-
-print("sum is equal to:",sum+1)
--- assignment_1/ques_5.py
+++
@@ -1,6 +0,0 @@
-x = int(input("enter a year:"))
-y = int(input("enter another year:"))
-
-for i in range(x,y+1):
-    if ((i%100==0) and (i%400==0)) or ((i%100 != 0) and (i%4==0)):
-            print(i," is a leap year")
